22330805f8911cabe18ceee765b6f52d22822740
hayone1 <boluwatife.morawo@stu.cu.edu.ng>
2024-02-27 21:50:28 +0100

    update flow

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -9,7 +9,8 @@ def test_get_full_path(path_name):
     assert os.path.isabs(fullPath)
 
 @pytest.mark.parametrize("zip_url", 
-    ["https://www.learningcontainer.com/wp-content/uploads/2020/05/sample-zip-file.zip"])
+    ["https://www.learningcontainer.com/wp-content/uploads/2020/05/sample-zip-file.zip",
+     "https://github.com/hayone1/MongoPrismSync/releases/download/untagged-38b2ab9338a6bbbcbb17/templates_v1alpha1.zip"])
 def test_download_and_extract_zip(zip_url):
     folder = utils.get_full_path("tests/debug/outputs/templates")
     print("Fullpath ", folder)
